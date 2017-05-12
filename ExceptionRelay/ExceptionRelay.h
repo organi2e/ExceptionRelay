37d4bd5a7114f7b3b632d5babffb73d22dc6e935
kotan.kn <kotan.kn@gmail.com>
2017-05-12 15:19:51 +0900

    Initial Commit

---
+++ ExceptionRelay/ExceptionRelay.h
@@ -0,0 +1,19 @@
+//
+//  ExceptionRelay.h
+//  ExceptionRelay
+//
+//  Created by Kota Nakano on 2017/05/12.
+//  Copyright © 2017 Kota Nakano. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ExceptionRelay.
+FOUNDATION_EXPORT double ExceptionRelayVersionNumber;
+
+//! Project version string for ExceptionRelay.
+FOUNDATION_EXPORT const unsigned char ExceptionRelayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExceptionRelay/PublicHeader.h>
+
+
